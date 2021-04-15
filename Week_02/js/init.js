4b8bf97628c7ed0dc188395427b3a1d5168962e3
Wny-Duong <wilny.duong@gmail.com>
2021-04-14 22:48:06 -0700

    initial commit with basic parts of the assignment done, hoping to flesh out some stuff to make it look nice

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -4,14 +4,41 @@ const name = "Albert";
 console.log("Hiya");
 
 // JavaScript const variable declaration
-const coolMap = L.map('map').setView([34.0709, -118.444], 15);
+const coolMap = L.map('map').setView([33.70928098530632, -117.95396310155171], 9);
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(coolMap);
 
-//JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(coolMap)
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
-		.openPopup();
+fetch("js/map1.geojson")
+    .then(response => {
+        return response.json();
+    })
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+		L.geoJSON(data, {
+			style: function (feature) {
+				return {color: feature.properties.color};
+			}
+		}).bindPopup(function (layer) {
+			return layer.feature.properties.place + "<br>" + layer.feature.properties.status;
+		}).addTo(coolMap);
+    });
+
+function customMarker (feature, latlng) {
+    return L.circleMarker(latlng, { color: feature.properties.color })
+  }
+  
+  // create an options object
+  let myLayerOptions = {
+    pointToLayer: customMarker
+  }
+// the leaflet method for adding a geojson
+
+//Create new markers for the following locations
+//Crystal Cove Trail
+//Borrego Springs Park
+//Jeffrey Open Space Trail
+//Huntington DOg Beach Trail
+//Bolsa Chica Ecological Reserve Trail
